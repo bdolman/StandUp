1a0d1aecebca9f784dfcfcd7fb79c7ff350f3d37
Ben Dolman <ben.dolman@gmail.com>
2015-11-16 22:05:50 -0700

    Initial Commit

---
+++ GeekDesk/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GeekDesk
+//
+//  Created by Ben Dolman on 11/16/15.
+//  Copyright © 2015 Hit Labs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
