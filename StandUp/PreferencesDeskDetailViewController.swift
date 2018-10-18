20318c47086c70c699a26c6969a7ffd24681d7af
Ben Dolman <ben.dolman@gmail.com>
2018-10-18 17:34:55 -0600

    WIP on a complete app overhaul that will support multiple desks and presets

---
+++ StandUp/PreferencesDeskDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesDeskDetailViewController.swift
+//  StandUp
+//
+//  Created by Ben Dolman on 10/18/18.
+//  Copyright © 2018 Hit Labs. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesDeskDetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ StandUp/PreferencesViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PreferencesViewController.swift
+//  StandUp
+//
+//  Created by Ben Dolman on 10/18/18.
+//  Copyright © 2018 Hit Labs. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesViewController: NSViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBAction func addDesk(_ sender: Any) {
+    }
+    
+    @IBAction func removeDesk(_ sender: Any) {
+    }
+    
+    override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
+        
+    }
+}
